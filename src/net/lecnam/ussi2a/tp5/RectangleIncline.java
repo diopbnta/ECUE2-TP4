3a265fddfaae3a63ec23e6af98fea34d1aaf3c35
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-22 14:38:05 +0200

    Exercice 6

---
+++ src/net/lecnam/ussi2a/tp5/RectangleIncline.java
@@ -0,0 +1,15 @@
+package net.lecnam.ussi2a.tp5;
+
+public class RectangleIncline extends Rectangle{
+
+    double angle;
+
+    public RectangleIncline(double longueur, double largeur, Point point, double angle){
+
+        super(longueur,largeur,point);
+        this.angle = angle;
+
+    }
+
+
+}
